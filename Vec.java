84cf0e67a813d444daeb368fdd0db5c3213c9cc1
Ethan Kempenich <kempenie@augsburg.edu>
2023-03-21 10:40:04 -0500

    Finished comments

--- Vec.java
+++ Vec.java
@@ -1,21 +1,21 @@
 /*
- * Vector.java
+ * Vec.java
  */
 
  /**
-  * Contains information about Vectors
+  * Contains information about Vecs
   * @author Ethan Kempenich
   * Template for code from https://rosettacode.org/wiki/Boids/Java
   */
 public class Vec {
-    /** x-coordinate of the vector */
+    /** x-coordinate of the vec */
     private int x;
-    /** y-coordinate of the vector */
+    /** y-coordinate of the vec */
     private int y;
 
-    /** Creates a vector
-     * @param x x-coordinate of the vector
-     * @param y y-coordinate of the vector
+    /** Creates a vec
+     * @param x x-coordinate of the vec
+     * @param y y-coordinate of the vec
      */
     public Vec(int x, int y){
         this.x = x;
@@ -35,41 +35,41 @@ public class Vec {
         return y;
     }
 
-    /** Adds two vectors together
-     * @param v Vector to be added to this vector
+    /** Adds two vecs together
+     * @param v Vec to be added to this vec
      */
     public void add(Vec v){
         x += v.x;
         y += v.y;
     }
 
-    /** Subtracts a vector's coordinates from this vector's
-     * @param v Vector whose coordinates are to be subtracted from this vector's
+    /** Subtracts a vec's coordinates from this vec's
+     * @param v Vec whose coordinates are to be subtracted from this vec's
      */
     public void subtract(Vec v){
         x -= v.x;
         y -= v.y;
     }
 
-    /** Multiplies two vectors
-     * @param v Vector to be multiplied by this vector
+    /** Multiplies two vecs
+     * @param v Vec to be multiplied by this vec
      */
     public void multiply(int val){
         x *= val;
         y *= val;
     }
 
-    /** Divides this vector's coordinates by another vector's
-     * @param v Vector by whose coordinates this vector's coordinates will be divided
+    /** Divides this vec's coordinates by another vec's
+     * @param v Vec by whose coordinates this vec's coordinates will be divided
      */
     public void divide(int val){
         x /= val;
         y /= val;
     }
 
-    /** Calculates the distance between this vector and another
-     * @param v Vector whose distance from this vector is being calculated
-     * @return Distance between the two vectors
+    /** Calculates the distance between this vec and another
+     * @param v Vec whose distance from this vec is being calculated
+     * @return Distance between the two vecs
      */
     public double distance(Vec v){
         return Math.sqrt(Math.pow(x - v.x,2) + Math.pow(y - v.y,2));
